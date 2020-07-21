965e8fe342089e853d398207b103998ba5867dbc
jod35 <jodestrevin@gmail.com>
2020-07-21 15:27:10 -0700

    first db migration

--- app/config.py
+++ app/config.py
@@ -4,5 +4,9 @@ base_dir=os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'youcantguessme' 
-    SQLALCHEMY_DATABASE_URI=os.path.join(base_dir,'app.db')
+    # SQLALCHEMY_DATABASE_URI='sqlite://'+ os.path.join(base_dir,'app.db')
+    
+
+    #for windows, I am on windows
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///C:/Users/Jonathan/coding/migroblog/site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
